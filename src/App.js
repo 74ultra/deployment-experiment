0041e1d61135e46cdc6adffeaa800829fb00167a
74ultra <r.barnhill@protonmail.com>
2019-11-04 17:51:23 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import cloudGear from './img/cloudgears.png';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>This is a test. This is not a real application</h2>
+      <img src={cloudGear} alt='Cloud gears'></img>
+    </div>
+  );
+}
+
+export default App;
